fe7ec4471940de10a2fe4d6dcaef2f462892f9f4
HeoYou <lov21y_@naver.com>
2020-09-03 18:28:44 +0900

    아아아 이것도 안풀려 더 쉬운방법을 찾아야 겠다.

--- 프로그래머스 문자열 압축.py
+++ 프로그래머스 문자열 압축.py
@@ -1,7 +1,34 @@
 def solution(s):
-    answer = "inf"
+    answer = 10000
+    answerStr = ''
+    answerLst = []
+    if len(s) < 3:
+        return len(s)
 
-    while 
+    for i in range(1, len(s) // 2 + 1):
+        if len(s) % i == 0:
+            count = 1
+            saveStr = s[:i]
 
+            for j in range(2, len(s)):
 
+                print(s[i * (j - 1) : i * j])
+                if saveStr == s[i * (j - 1) : i * j]:
+                    count += 1
+                else:
+                    answerLst.append([count, saveStr])
+                    saveStr = s[i * (j - 1) : i * j]
+                    count = 1
+                print(i, j, answerLst)
+            for count, S in answerLst:
+                if count == 1:
+                    answerStr += S
+                else:
+                    answerStr += str(count) + S
+            answer = min(answer, len(answerStr))
+            print(answerLst)
+            answerStr = ''
+            answerLst = []
     return answer
+
+print(solution("aabbaccc"))
