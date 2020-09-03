1d41f8fc830133c6183b70ad0587c396c89930e7
HeoYou <lov21y_@naver.com>
2020-09-03 16:16:06 +0900

    이분탐색 강의듣고 코딩한거다. 나중에 한번 더 봐야겠다.

--- Q2110 공유기 설치.py
+++ Q2110 공유기 설치.py
@@ -1,9 +1,26 @@
 import sys
 N, C = map(int, sys.stdin.readline().split())
-
 data = []
 for i in range(N):
     data.append(int(sys.stdin.readline().strip()))
-data.sort()
+data = sorted(data)
+answer = 0
 
-print(data)
+start = data[1] - data[0]
+end = data[-1] - data[0]
+
+while start <= end:
+    mid = (start + end) // 2
+    value = data[0]
+    count = 1
+    for i in range(1, len(data)):
+        if data[i] >= value + mid:
+            value = data[i]
+            count += 1
+    if count >= C:
+        start = mid + 1
+        answer = mid
+    else:
+        end = mid - 1
+
+print(answer)
