ea744e3c3a963e56d4ec98fd8ddbbf48dcd41ffd
HeoYou <lov21y_@naver.com>
2020-09-04 11:25:55 +0900

    자잘한 수정

--- Q1541 잃어버린 괄호.py
+++ Q1541 잃어버린 괄호.py
@@ -1,7 +1,7 @@
 data = input().split('-')
-answer = sum(list(map(int, data[0].split('+'))))
+answer = sum(map(int, data[0].split('+')))
 data.pop(0)
 for i in data:
-    answer -= sum(list(map(int, i.split('+'))))
+    answer -= sum(map(int, i.split('+')))
 print(answer)
 
