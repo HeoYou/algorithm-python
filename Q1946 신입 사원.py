3fac02675fcadf8ffb7600fa221f1d101b2582b3
HeoYou <lov21y_@naver.com>
2020-09-07 01:09:09 +0900

    이문제도 이해 잘못해서 방황하느라 식나을 많이보냄

--- Q1946 신입 사원.py
+++ Q1946 신입 사원.py
@@ -1,9 +1,22 @@
+import sys
 t = int(input())
 
 for _ in range(t):
     n = int(input())
     data = []
-    
+    count = 1   
     for i in range(n):
-        data.append(list(map(int, input().split())))
-    print(data)
+        data.append(list(map(int, sys.stdin.readline().split())))
+
+    data = sorted(data)
+    maxData = data[0][1]
+
+    for i in range(n):
+        if maxData == n + 1:
+            break
+        else:
+            if maxData > data[i][1]:
+                maxData = data[i][1]
+                count += 1
+        
+    print(count)
