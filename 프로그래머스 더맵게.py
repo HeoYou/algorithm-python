28353977438f26f70314ae7908c12326809aba23
HeoYou <lov21y_@naver.com>
2020-09-08 19:56:10 +0900

    힙 힌트가지고 하려고하는데 왜안되는지 잘모르겠다.. 머리좀 식히고 내일다시해봐야겠다.

--- 프로그래머스 더맵게.py
+++ 프로그래머스 더맵게.py
@@ -15,3 +15,34 @@
 #     return answer
 
 # print(solution([1, 2, 3, 9, 10, 12], 7))
+
+
+import heapq
+
+def solution(scoville, K):
+
+    heap = scoville
+    heapq.heapify(heap)
+    answer = 0
+    sum = 0
+
+    if heap[0] >= K:
+        return 0
+
+    while len(heap) > 1:
+        sum = 0
+        sum += heapq.heappop(heap)
+        sum += heapq.heappop(heap) * 2
+        if sum >= K:
+            answer += 1
+            return answer
+        heapq.heappush(heap, sum)
+        answer += 1
+
+    if len(heap) <= 1:
+        return -1
+
+
+    return answer
+
+print(solution([1, 1, 100], 5))
