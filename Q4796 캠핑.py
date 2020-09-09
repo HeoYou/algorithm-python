6756a75e56f66da06cb34f46efe9801f5392a9c1
HeoYou <lov21y_@naver.com>
2020-09-09 16:37:37 +0900

    나머지 연산으로 수정

--- Q4796 캠핑.py
+++ Q4796 캠핑.py
@@ -3,5 +3,5 @@ while True:
     a, b, c = map(int, input().split())
     if a == 0 and b == 0 and c == 0:
         break
-    print("Case {}: {}".format(tc, c // b * a + a if c - (b * (c // b)) >= a else c // b * a + (c - (b * (c // b)))))
+    print("Case {}: {}".format(tc, c // b * a + a if c % b >= a else c // b * a + (c % b)))
     tc += 1
