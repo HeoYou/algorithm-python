ccafdf7d6fd67706325921b33a566a60139b073f
HeoYou <lov21y_@naver.com>
2020-09-11 22:46:27 +0900

    다시풀어보았다.

--- Q2606 바이러스.py
+++ Q2606 바이러스.py
@@ -1,23 +1,46 @@
-import sys
-graph = {i + 1 : [] for i in range(int(sys.stdin.readline()))}
+# import sys
+# graph = {i + 1 : [] for i in range(int(sys.stdin.readline()))}
 
-def dfs(graph, start):
-    stack = [start]
+# def dfs(graph, start):
+#     stack = [start]
+#     visit = []
+
+#     while stack:
+#         node = stack.pop()
+#         if node not in visit:
+#             visit.append(node)
+#             stack.extend(graph[node])
+#     return visit
+# for i in range(int(sys.stdin.readline())):
+#     a, b = map(int, sys.stdin.readline().split())
+#     graph[a].append(b)
+#     graph[b].append(a)
+# answer = 0
+
+# for i in range(len(graph)):
+#     answer = max(answer, len(dfs(graph, i + 1)))
+
+# print(answer - 1)
+
+n = int(input())
+l = int(input())
+
+graph = [[] for i in range(n + 1)]
+for i in range(l):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs():
+    stack = [1]
     visit = []
 
     while stack:
         node = stack.pop()
         if node not in visit:
-            visit.append(node)
             stack.extend(graph[node])
-    return visit
-for i in range(int(sys.stdin.readline())):
-    a, b = map(int, sys.stdin.readline().split())
-    graph[a].append(b)
-    graph[b].append(a)
-answer = 0
+            visit.append(node)
 
-for i in range(len(graph)):
-    answer = max(answer, len(dfs(graph, i + 1)))
+    return len(visit)
 
-print(answer - 1)
+print(dfs() - 1)
