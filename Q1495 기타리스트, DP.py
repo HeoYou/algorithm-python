110b812dc8d6b07aeb9e47923ef895fe567c3abb
HeoYou <lov21y_@naver.com>
2020-09-11 21:15:52 +0900

    dp

---
+++ Q1495 기타리스트, DP.py
@@ -0,0 +1,25 @@
+n, s, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+dp = [[False] * (m + 1) for _ in range(n + 1)]
+dp[0][s] = True
+
+for i in range(n):
+    for j in range(m + 1):
+        if dp[i][j] == True:
+            # print(j, arr[i])
+            # for x in range(len(dp)):
+            #     print(dp[x])
+            if j + arr[i] <= m:
+                dp[i + 1][j + arr[i]] = True
+            if j - arr[i] >= 0:
+                dp[i + 1][j - arr[i]] = True
+
+for i in range(m, -1, -1):
+    if dp[-1][i] == True:
+        print(i)
+        break
+
+if True not in dp[-1]:
+    print(-1)
+            
