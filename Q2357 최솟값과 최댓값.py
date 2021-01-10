01a5f99aae6f5ec6511f849d30a2a39d2c3d225b
HeoYou <lov21y_@naver.com>
2021-01-10 19:35:11 +0900

    속도문제때문에 골머리 썩고있다. 세그먼트 트리를 한번 공부해야겠다.

--- Q2357 최솟값과 최댓값.py
+++ Q2357 최솟값과 최댓값.py
@@ -1,15 +1,27 @@
 import sys
 
 N, M = map(int, input().split())
-dataLst = []
-minArr, maxArr = [], []
+dataLst = [0] * N
+minArr, maxArr = [0] * M, [0] * M
+
+def minMax(arr):
+    minNum, maxNum = 1000000000, 1
+
+    for i in range(len(arr)):
+        minNum = minNum if arr[i] > minNum else arr[i]
+        maxNum = maxNum if arr[i] < maxNum else arr[i]
+    return minNum, maxNum
+
+
 for i in range(N):
-    dataLst.append(int(sys.stdin.readline()))
+    dataLst[i] = int(sys.stdin.readline())
 
 for i in range(M):
     a, b = map(int, sys.stdin.readline().split())
-    minArr.append(min(dataLst[a - 1:b]))
-    maxArr.append(max(dataLst[a - 1:b]))
+    minNum, maxNum = minMax(dataLst[a - 1:b])
+    minArr[i] = minNum
+    maxArr[i] = maxNum
+    
 
 for i in range(M):
     print(minArr[i], end=" ")
