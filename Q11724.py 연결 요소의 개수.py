c8f0bd95b2513b8d069d8d2ab1668b4d8a89daa4
HeoYou <lov21y_@naver.com>
2021-01-10 18:46:37 +0900

    pypy3로 제출하니까 통과한다. 다른방법 알아봐야것다. 그래프이론

--- Q11724.py 연결 요소의 개수.py
+++ Q11724.py 연결 요소의 개수.py
@@ -9,7 +9,7 @@ answer = 0
 
 def dfs(node):
     visit[node] = True
-    # print(visit)
+
     for i in graph[node]:
         if not visit[i]:
             dfs(i)
@@ -18,12 +18,10 @@ for i in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-# print(graph)
+
 
 for i in range(1, N + 1):
-
     if not visit[i]:
-        #print(1234,visit)
         answer += 1
         dfs(i)
 
