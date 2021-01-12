56f77f89577781f1ca6a030a4f2dc2a463ae603a
HeoYou <lov21y_@naver.com>
2021-01-12 23:45:15 +0900

    아 골때린다 백준에서 heap을 막아둔거같다 pypy3에서는 작동하니 문제를 풀긴했다.

--- Q1202 보석 도둑.py
+++ Q1202 보석 도둑.py
@@ -1,22 +1,48 @@
+# import sys
+# N, K = map(int, input().split())
+
+# answer = 0
+# jewelLst, bagLst= [], []
+# for i in range(N):
+#     jewelLst.append(list(map(int, sys.stdin.readline().split())))
+# jewelLst = sorted(jewelLst, key = lambda x : x[1], reverse = True)
+
+# for i in range(K):
+#     bagLst.append(int(sys.stdin.readline()))
+# bagLst = sorted(bagLst, reverse = True)
+
+# for i in range(len(bagLst)):
+#     for j in range(len(jewelLst)):
+#         if bagLst[i] >= jewelLst[j][0]:
+#             answer += jewelLst[j][1]
+#             jewelLst.pop(j)
+#             break
+
+
+# print(answer)
+
+import heapq
 import sys
+
+
 N, K = map(int, input().split())
 
 answer = 0
-jewelLst, bagLst= [], []
-for i in range(N):
-    jewelLst.append(list(map(int, sys.stdin.readline().split())))
-jewelLst = sorted(jewelLst, key = lambda x : x[1], reverse = True)
+jewelLst, bagLst, tmp = [], [0] * K, []
+for _ in range(N):
+    heapq.heappush(jewelLst, list(map(int, sys.stdin.readline().split())))
 
 for i in range(K):
-    bagLst.append(int(sys.stdin.readline()))
-bagLst = sorted(bagLst, reverse = True)
+    bagLst[i] = int(sys.stdin.readline())
+bagLst.sort()
 
-for i in range(len(bagLst)):
-    for j in range(len(jewelLst)):
-        if bagLst[i] >= jewelLst[j][0]:
-            answer += jewelLst[j][1]
-            jewelLst.pop(j)
-            break
-
-
+for i in bagLst:
+    while jewelLst and i >= jewelLst[0][0]:
+        heapq.heappush(tmp, -heapq.heappop(jewelLst)[1])
+    if tmp:
+        answer += -heapq.heappop(tmp)
+    elif not jewelLst:
+        break
+    
 print(answer)
+
