4b167cd10e66ca8d24d6471a99e300064e1b3db1
HeoYou <lov21y_@naver.com>
2021-02-03 00:29:27 +0900

    옛날에 못풀었던 문제 점화식 새우는데 좀 오래걸렸다..

--- Q11053 가장 긴 증가하는 부분 수열.py
+++ Q11053 가장 긴 증가하는 부분 수열.py
@@ -1,7 +1,16 @@
 n = int(input())
 dp = list(map(int, input().split()))
-count = 1
-for i in range(1, n):
+answer = [0] * (n + 1)
+
+for i in range(n):
+    minNum = 0
+    for j in range(i + 1):
+        if dp[i] > dp[j]:
+            if minNum < answer[j]:
+                minNum = answer[j]
+    answer[i] = minNum + 1
+    
 
 
-print(count)
+
+print(max(answer))
