de2a0abd014e41b60015a8a6640f2618d902fbb9
HeoYou <lov21y_@naver.com>
2021-08-13 23:51:51 +0900

    이것도 ...

---
+++ Q4949 균형잡힌 세상(스택).py
@@ -0,0 +1,35 @@
+cmd = ''
+answer = []
+while True:
+    cmd = input()
+    if cmd == '.':
+        break
+    stack = []
+
+    for s in cmd:
+
+        if s == '(':
+            stack.append('(')
+        elif s == '[':
+            stack.append('[')
+        elif s == ')':
+            if len(stack):
+                if stack.pop() == '[':
+                    print('no')
+                    break
+
+        elif s == ']':
+            if len(stack):
+                if stack.pop() == '(':
+                    print('no')
+                    break
+        print(stack)
+    if len(stack):
+        print('no')
+        answer.append('no')
+    else:
+        print('yes')
+        answer.append('yes')
+
+print(answer)
+
