335eb03eced9b03483929294a0fbb3fbc0efacd6
HeoYou <lov21y_@naver.com>
2021-08-16 00:20:06 +0900

    dfs로 수정후 바로 풀림 이상한거 커밋해서 다시함

--- Q1182 부분수열의 합(브루트 포스).py
+++ Q1182 부분수열의 합(브루트 포스).py
@@ -2,15 +2,35 @@
 # 부분수열의 정의를 정확히 알지 못하고 있음 
 # 부분수열이란 연속된게 아니라 그냥 상대적 순서만 가지고 있으면 된다..
 # 이 문제를 해결하려면 재귀적인 풀이가 필요해 보인다. 근데 도저히 감이 안잡힌다.
+# dfs 로 접근하면 가능해보임 
 
 n, m = map(int, input().split(' '))
 lst = list(map(int, input().split(' ')))
 
 answer = 0
 
-for i in range(n):
-    for j in range(i + 1, n):
-        if sum(lst[i:j]) == m:
-            answer += 1
-answer += lst.count(m)
+
+def dfs(s, p):
+    global answer
+
+    if p >= n:
+        return
+    s += lst[p]
+    if s == m:
+        answer += 1
+
+    dfs(s - lst[p], p + 1)
+    dfs(s, p + 1)
+dfs(0, 0)
+
 print(answer)
+
+
+
+
+# for i in range(n):
+#     for j in range(i + 1, n):
+#         if sum(lst[i:j]) == m:
+#             answer += 1
+# answer += lst.count(m)
+# print(answer)
