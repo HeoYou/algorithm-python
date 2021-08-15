530e24ef4a997fc2360c11c3f02b26f0d981d64b
HeoYou <lov21y_@naver.com>
2021-08-15 19:21:09 +0900

    코드 인터넷에 가져왔는데 뭐가 다른건지 디지게 모르겠다.

--- Q1018 체스판 다시 칠하기.py
+++ Q1018 체스판 다시 칠하기.py
@@ -21,29 +21,53 @@
 
 # print(min(minCountB, minCountW))
 
-n, m = map(int, input().split(' '))
-lst = []
-answer = []
-for i in range(n):
-    lst.append(input())
-wb, bw = 'WB', 'BW'
-for i in range(n - 7):
-    for j in range(m - 7):
-        w, b = 0, 0
-        for x in range(i, i + 8):
-            for y in range(j, j + 8):
-                if (x + y) % 2 == 0:
-                    if lst[x][y] != 'W': w += 1  
-                    if lst[x][y] != 'B': b += 1
-                else:
-                    if lst[x][y] != 'B': w += 1  
-                    if lst[x][y] != 'W': b += 1
-    answer.append(w)  
-    answer.append(b)
+# n, m = map(int, input().split(' '))
+# lst = []
+# answer = []
+# for i in range(n):
+#     lst.append(input())
+# wb, bw = 'WB', 'BW'
 
-print(min(answer))
+# for i in range(n - 7):
+#     for j in range(m - 7):
+#         w, b = 0, 0
+#         for x in range(i, i + 8):
+#             for y in range(j, j + 8):
+#                 if (x + y) % 2 == 0:
+#                     if lst[x][y] != 'W': w += 1  
+#                     if lst[x][y] != 'B': b += 1
+#                 else:
+#                     if lst[x][y] != 'B': w += 1  
+#                     if lst[x][y] != 'W': b += 1
+#     answer.append(w)  
+#     answer.append(b)
+
+# print(min(answer))
 
 
+n, m = map(int, input().split())
+l = []
+mini = []
+
+for _ in range(n):
+    l.append(input())
+
+for a in range(n - 7):
+    for i in range(m - 7):
+        idx1 = 0
+        idx2 = 0
+        for b in range(a, a + 8):
+            for j in range(i, i + 8): 
+                if (j + b)%2 == 0:
+                    if l[b][j] != 'W': idx1 += 1  
+                    if l[b][j] != 'B': idx2 += 1
+                else :
+                    if l[b][j] != 'B': idx1 += 1
+                    if l[b][j] != 'W': idx2 += 1
+        mini.append(idx1)                        
+        mini.append(idx2)                          
+
+print(min(mini))  
 
 
 
