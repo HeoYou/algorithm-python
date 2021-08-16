dbfebd4160ea42a53e9c7ee40f781b2bfff5af98
HeoYou <lov21y_@naver.com>
2021-08-16 23:02:55 +0900

    우선 시간초과가 난다. 데크를 안쓰긴했는데 이것이 문제인가 아마 수정해봐야 알것같다

--- Q2589 보물섬(BFS).py
+++ Q2589 보물섬(BFS).py
@@ -1,31 +1,32 @@
+
 n, m = map(int, input().split())
 
 dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
 
 lst = [list(input()) for _ in range(n)]
-
+answer = 0
 for x in range(n):
     for y in range(m):
         visit = [[0] * m for j in range(n)]
-        for i in visit:
-            print(i)
         stack = [[x, y, 1]]
-        print(stack)
-        
+        if lst[x][y] == 'W':
+            continue
+
         while stack:
             sx, sy, d = stack.pop(0)
             visit[sx][sy] = d
             
             for i in range(4):
                 nx, ny = sx + dx[i], sy + dy[i]
-                if visit[x][y] == 0 and 0 <= x < n and 0 <= y < m:
-                    stack.append(nx, ny, d + 1)
-    for i in visit:
-        print(i)
-    print()
+                if 0 <= nx < n and 0 <= ny < m and visit[nx][ny] == 0 and lst[nx][ny] == 'L':
+                    stack.append([nx, ny, d + 1])
 
+        answer = max(answer, max(map(max, visit)))
 
-
+        for i in visit:
+            print(i)
+        print()
+print(answer - 1)
 # def bfs(x, y, visit):
 
 #     visit[x][y] = 0
