c0da1680faffddc9d0c3f939c7ec089b768f580d
HeoYou <lov21y_@naver.com>
2021-08-17 18:13:53 +0900

    엣지케이스를 못찾는거 같다. 다른 글들 보면 유니온 파인드를 이용해서 찾는데 나는 그냥 그래프 방문 기록을 이용해서 풀라고 했다 그러나 안된다.

--- Q1976 여행 가자.py
+++ Q1976 여행 가자.py
@@ -1,24 +1,69 @@
+# 옛날에 짯던 코드인데 이방법으로 안되는게 확실하다
+# 아마 다른도시를 거쳐 갈 수 있다는걸 간과한 결과인거같은데
+# 금방 풀 수 있을것 같다
+
+# import sys
+
+# n = int(sys.stdin.readline())
+# m = int(sys.stdin.readline())
+
+# city = []
+# root = []
+
+# for _ in range(n):
+#     city.append(list(map(int, sys.stdin.readline().split())))
+# root = list(map(int, sys.stdin.readline().split()))
+
+# flag = True
+
+# for i in range(m - 1):
+#     if root[i] == root[i + 1]:
+#         continue
+#     if city[root[i] - 1][root[i + 1] - 1] == 0:
+#         falg = False
+#         break
+# if flag:
+#     print("YES")
+# else:
+#     print("NO")
+
+
+# 첫번째 방문지를 기준으로 그래프 탐색을 한다
+# 그래프로 탐색한 곳을 기록하고 나중에 체크하는 식으로
+
+
+# 엣지 케이스가 나온 것 같다 안풀리네
 import sys
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
-m = int(sys.stdin.readline())
+n, m = int(input()), int(input())
+graph = [[] for i in range(n + 1)]
+visit = [0] * (n + 1)
 
-city = []
-root = []
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    for j in range(len(tmp)):
+        if tmp[j]:
+            graph[i + 1].append(j + 1)
 
-for _ in range(n):
-    city.append(list(map(int, sys.stdin.readline().split())))
-root = list(map(int, sys.stdin.readline().split()))
+lst = list(map(int, input().split()))
 
-flag = True
+stack = [lst[0]]
 
-for i in range(m - 1):
-    if root[i] == root[i + 1]:
-        continue
-    if city[root[i] - 1][root[i + 1] - 1] == 0:
-        falg = False
+while stack:
+
+    node = stack.pop()
+    visit[node] = 1
+
+    for i in graph[node]:
+        if visit[i] == 0:
+            stack.append(i)
+
+for i in lst:
+    if visit[i] == 0:
+        print('NO')
         break
-if flag:
-    print("YES")
-else:
-    print("NO")
+print('YES')
+
+
+
