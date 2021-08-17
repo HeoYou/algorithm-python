5b4f856c6426f96995a09a8a4dc1bea725c62bcb
HeoYou <lov21y_@naver.com>
2021-08-17 14:59:33 +0900

    투포인터로 풀었는데 문제가 있나보다 이거 더 확실하게 풀어보자.

--- 프로그래머스 보석쇼핑.py
+++ 프로그래머스 보석쇼핑.py
@@ -1,15 +1,43 @@
+# 문제 다시한번 풀어보려고 들어왔는데
+# 왜 이딴식으로 풀어논지 전혀 감이 안잡힌다.
+
+# def solution(gems):
+#     answer = []
+#     start, end = 0, 0
+#     gemCount = len(set(gems))
+
+#     while len(set(gems[start:end])) != gemCount:
+#         end += 1
+    
+#     while len(set(gems[start:end])) == gemCount:
+#         start += 1
+
+
+#     return [start, end]
+
 def solution(gems):
     answer = []
-    start, end = 0, 0
-    gemCount = len(set(gems))
+    left, right = 0, 0
+    total = len(gems)
+    dic = {}
+    for i in set(gems):
+        dic[i] = 0
+    dic[gems[0]] += 1
+    print(dic)
 
-    while len(set(gems[start:end])) != gemCount:
-        end += 1
-    
-    while len(set(gems[start:end])) == gemCount:
-        start += 1
+    while right != total - 1:
+
+        if min(dic.values()) == 0:
+            right += 1
+            dic[gems[right]] += 1
+
+        else:
+            answer.append([left + 1, right + 1, right - left])
+            dic[gems[left]] -= 1
+            left += 1
+        print(dic, left, right)
+    answer = sorted(answer, key = lambda x : x[2])
+    return [answer[0][0], answer[0][1]] if len(answer) else [1, len(gems)]
 
 
-    return [start, end]
-
-print(solution(["DIA", "EM", "EM", "RUB", "DIA"]))
+print(solution(["ZZZ", "YYY", "NNNN", "YYY", "BBB"]))
