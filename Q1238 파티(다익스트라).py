eb99eb3734b6aaf3e3c2070e39b821f929793ce5
HeoYou <lov21y_@naver.com>
2021-08-20 10:12:38 +0900

    다익스트라는 BFS를 사용해야하는줄 알았는데.. 거리를 기준으로 힙을 사용하면 훨씬 빠르다. 그러나 다른사람들 풀이에 비해 엄청 느리단걸 알 수 있다.

---
+++ Q1238 파티(다익스트라).py
@@ -0,0 +1,36 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n, e, x = map(int, input().split())
+
+graph = {}
+d = [[10000000] * (n + 1) for i in range(n + 1)]
+
+for i in range(1, n + 1):
+    graph[i] = []
+for e in range(e):
+    f, s, v = map(int, input().split())
+    graph[f].append([s, v])
+
+
+for i in range(1, n + 1):
+
+    queue = []
+    heapq.heappush(queue, (0, i))
+
+    while queue:
+
+        v, node = heapq.heappop(queue)
+        for nn, nv in graph[node]:
+            nv = nv + v
+            if nv < d[i][nn]  and nn != i:
+                d[i][nn] = nv
+                heapq.heappush(queue, (nv, nn))
+
+answer = 0
+for i in range(1, n + 1):
+    if i != x:
+        answer = max(answer, d[i][x] + d[x][i])
+print(answer)
+                
