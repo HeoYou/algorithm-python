df808e7a67a9e496cc11358d8aa5b71a3d9f6016
HeoYou <lov21y_@naver.com>
2021-08-29 22:10:59 +0900

    이걸 통과하네...

--- Q15649 N과 M(1).py
+++ Q15649 N과 M(1).py
@@ -1,10 +1,19 @@
-N, M = map(int, input().split())
+# 이걸 통과하네... 시간제한 걸릴줄
 
-visit = [0] * N
-out = []
-print(range(N))
+import sys
+sys.setrecursionlimit(10 ** 6)
 
-def dfs(depth):
-    if depth == M:
-        print(' '.join(map(str, out)))
+n, m = map(int, input().split())
+
+def bfs(lst):
+    if len(lst) == m:
+        print(' '.join(lst))
         return
+    
+    for i in range(1, n + 1):
+        # in에서 걸릴줄 알았는데
+        if str(i) not in lst:
+            bfs(lst + [str(i)])
+
+bfs([])
+
