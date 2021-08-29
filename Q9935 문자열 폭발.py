028bd7a49e71ee31da519013a393f3d678f444f1
HeoYou <lov21y_@naver.com>
2021-08-29 21:17:32 +0900

    del() pop() 이거슬 속도를 알아야했다.

--- Q9935 문자열 폭발.py
+++ Q9935 문자열 폭발.py
@@ -1,6 +1,43 @@
-a = input()
-b = input()
-while a.count(b) > 0:
-    a = a.replace(b, '')
+# 이렇게 풀리면 개꿀 골4가 아닐꺼야...
+# a = input()
+# b = input()
+# while a.count(b) > 0:
+#     a = a.replace(b, '')
 
-print(a if a else 'FRULA')
+# print(a if a else 'FRULA')
+
+
+# 1번째 풀이 시간초과 
+# s = list(input())
+# comp = list(input())
+
+# stack = []
+# last_comp = comp[-1]
+# len_comp = len(comp)
+
+# for i in s:
+#     stack.append(i)
+#     if last_comp == stack[-1]:
+#         if len(stack) >= len_comp:
+#             if stack[-len_comp:] == comp[:]:
+#                 stack = stack[:-len_comp]
+
+
+# print(''.join(stack) if stack else 'FRULA')
+
+# 그냥 리스트를 복사하는게 문제였따. del 을쓰자
+s = list(input())
+comp = list(input())
+
+stack = []
+last_comp = comp[-1]
+len_comp = len(comp)
+
+for i in s:
+    stack.append(i)
+    if last_comp == stack[-1]:
+        if stack[-len_comp:] == comp[:]:
+            del stack[-len_comp:]
+
+
+print(''.join(stack) if stack else 'FRULA')
