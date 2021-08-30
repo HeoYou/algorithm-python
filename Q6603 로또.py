71aec130049249380d9bb5f5ac3b4da7684bd374
HeoYou <lov21y_@naver.com>
2021-08-31 00:00:44 +0900

    오타 다시커밋

--- Q6603 로또.py
+++ Q6603 로또.py
@@ -4,8 +4,8 @@ while True:
     items = list(map(int, input().split()))
     if len(items) == 1:
         break
-    del [0]
-    lst = sorted(list(combinations(items, 6)))
+    del items[0]
+    lst = list(combinations(items, 6))
     for i in lst:
         for j in sorted(list(i)):
             print(j, end=' ')
