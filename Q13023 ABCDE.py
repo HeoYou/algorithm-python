603160d892e474b61eea02d8cdef6e8c9cfc38b0
HeoYou <lov21y_@naver.com>
2021-09-02 16:28:23 +0900

    dfs 방식을 바꿔보았다., 왜틀리는지는 잘모름 좀더 알아봐야함 우선 테케는 다통과함

--- Q13023 ABCDE.py
+++ Q13023 ABCDE.py
@@ -1,3 +1,31 @@
+# n, m = map(int, input().split())
+
+# node = [[] for i in range(n)]
+# visit = [0] * n
+# for i in range(m):
+#     x, y = map(int, input().split())
+#     node[x].append(y)
+#     node[y].append(x)
+
+
+# for i in range(n):
+#     if visit[i] == 0:
+#         stack = [i]
+#         count = 0
+#         while stack:
+#             print(stack)
+#             n = stack.pop()
+#             visit[n] = 1
+#             count += 1
+#             if count >= 5:
+#                 print(1)
+#                 exit()
+#             for j in node[n]:
+#                 if visit[j] == 0:
+#                     stack.append(j)
+    
+# print(0)
+
 n, m = map(int, input().split())
 
 node = [[] for i in range(n)]
@@ -7,21 +35,19 @@ for i in range(m):
     node[x].append(y)
     node[y].append(x)
 
+def dfs(d, num):
+    global visit
+    visit[num] = 1
+    if d == 5:
+        print(1)
+        exit()
+    
+    for i in node[num]:
+        if visit[i] == 0:
+            dfs(d + 1, i)
 
 for i in range(n):
-    if visit[i] == 0:
-        stack = [i]
-        count = 0
-        while stack:
-            print(stack)
-            n = stack.pop()
-            visit[n] = 1
-            count += 1
-            if count >= 5:
-                print(1)
-                exit()
-            for j in node[n]:
-                if visit[j] == 0:
-                    stack.append(j)
-    
+    visit = [0] * n
+    dfs(1, i)
 print(0)
+
