f4273ba566efccc5a866928a61da9b73f427fa6e
HeoYou <lov21y_@naver.com>
2021-09-03 09:58:42 +0900

    옛날엔 왜 이걸 못풀었지..?

--- 03월 1주차 백신 별별별.py
+++ 03월 1주차 백신 별별별.py
@@ -1,6 +1,39 @@
-n, m = map(int, input().split(" "))
+import sys
+input = sys.stdin.readline
 
-a, b = [0 for _ in range(m)], [0 for _ in range(m)]
+n, m = map(int, input().split())
+
+nodes = [[] for i in range(n + 1)]
+visit = [0] * (n + 1)
+answer = [0, 0]
 
 for i in range(m):
-    a[i], b[i] = map(int, input().split(" "))
+    a, b = map(int, input().split())
+
+    nodes[a].append(b)
+    nodes[b].append(a)
+
+print(nodes)
+
+for i in range(1, n + 1):
+    if visit[i] == 0:
+
+        count = 1
+        stack = [i]
+
+        while stack:
+
+            node = stack.pop()
+            visit[node] = 1
+
+            for j in nodes[node]:
+                if visit[j] == 0:
+                    visit[j] = 1
+                    count += 1
+                    stack.append(j)
+            
+        if answer[1] < count:
+            answer[1] = count
+            answer[0] = i
+
+print(answer)
