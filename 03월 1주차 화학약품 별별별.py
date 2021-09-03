b2ecf61cfc150df199ecf8de9fa331f24fea0858
HeoYou <lov21y_@naver.com>
2021-09-03 11:22:50 +0900

    최대한 줄이고 줄이는데 ... ㅌ임아웃 어쩔거야..

--- 03월 1주차 화학약품 별별별.py
+++ 03월 1주차 화학약품 별별별.py
@@ -1,4 +1,20 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+input = sys.stdin.readline
+
+answer = 0
 n = int(input())
+dataLst = list(map(int, input().strip().split(" ")))
 
-dataLst = list(map(int, input().strip().split(" "))
+def dfs(lst, pos, flag):
+    global answer
+    if flag and lst == 0:
+        answer += 1
+    if pos == n:
+        return
+    dfs(lst + dataLst[pos], pos + 1, True)
+    dfs(lst, pos + 1, False)
 
+dfs(0, 0, False)
+print(answer)
