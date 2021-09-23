a46ff0e85303e9ea41edad1cf3c9a6a4918b89a7
HeoYou <lov21y_@naver.com>
2021-09-23 22:18:17 +0900

    중간에 틀리는데 이유는 모르겠다 너무 피곤쓰하다 우선 이;유부터 찾아봐야것다 시간초과는 아슬아슬하게 안나는것같은데

---
+++ Q2468 안전 영역(그래프).py
@@ -0,0 +1,35 @@
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+
+n = int(input())
+
+lst = [list(map(int, input().split())) for _ in range(n)]
+answer = 0
+
+def dfs(sx, sy, h):
+    global visit
+    stack = [[sx, sy]]
+
+    while stack:
+        x, y = stack.pop()
+        visit[x][y] = 1
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n:
+                if lst[nx][ny] > h and visit[nx][ny] == 0:
+                    stack.append([nx, ny])
+
+for hieght in range(1, 101):
+    count = 0
+    visit = [[0] * n for _ in range(n)]
+
+    for x in range(n):
+        for y in range(n):
+            if visit[x][y] == 0 and lst[x][y] > hieght:
+                count += 1
+                dfs(x, y, hieght)
+
+    answer = max(answer, count)
+
+print(answer)
+
+
