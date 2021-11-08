0310d4db4101a03aec953b472ac36164a60104b6
HeoYou <lov21y_@naver.com>
2021-11-09 00:28:40 +0900

    음... 잘모르겠어.. 내일 다시 고민

--- 프로그래머스 줄 서는 방법.py
+++ 프로그래머스 줄 서는 방법.py
@@ -1,6 +1,7 @@
 from itertools import permutations
+import math
 
-def solution(n, k):
+def solution_1(n, k):
     answer = []
     p = permutations([i for i in range(1, n + 1)])
     
@@ -10,5 +11,24 @@ def solution(n, k):
             return i
         count += 1
 
+def solution(n, k):
+    answer = []
+    numberLst = [i for i in range(1, n + 1)]
+
+    while(n != 0):
+        temp = math.factorial(n)
+        print('temp', temp)
+        index = k // temp
+        print('index', index)
+        k = k % temp
+        print('k', k)
+        if k == 0:
+            answer.append(numberLst.pop(index-1))
+        else:
+            answer.append(numberLst.pop(index))
+
+        n -= 1
+    return answer
+    
 
 print(solution(3, 5))
