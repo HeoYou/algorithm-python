6b9f7d1aa9182cb3fd28d5c98db7ff30b8d4db11
HeoYou <lov21y_@naver.com>
2022-01-06 16:02:28 +0900

    오랜만에 보니까 왜 안되는지 알거같다 하지만.. 백트래킹을 할줄 모른다.

--- Q1987 알파벳(DFS).py
+++ Q1987 알파벳(DFS).py
@@ -31,6 +31,7 @@ dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
 n, m = map(int, input().split())
 lst = [list(input()) for i in range(n)]
 count_lst = [[0] * m for i in range(n)]
+answer = []
 
 def dfs(x, y, c, visit):
     global count_lst
@@ -38,11 +39,15 @@ def dfs(x, y, c, visit):
     visit = copy.deepcopy(visit) 
     visit[lst[x][y]] = 1
 
+    flag = 0
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
         if 0 <= nx < n and 0 <= ny < m:
             if visit[lst[nx][ny]] == 0 and count_lst[nx][ny] <= c:
+                flag = 1
                 dfs(nx, ny, c + 1, visit)
+        if not flag:
+            answer
     for i in count_lst:
         print(i)
     print()
@@ -50,3 +55,34 @@ def dfs(x, y, c, visit):
 dic = {chr(i):0 for i in range(65, 91)}
 dfs(0, 0, 1, dic)
 print(count_lst)
+
+# import sys
+# import copy
+# from collections import deque
+# sys.setrecursionlimit(1000000)
+# dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+# n, m = map(int, input().split())
+# lst = [list(input()) for i in range(n)]
+# count_lst = [[0] * m for i in range(n)]
+
+# q = deque()
+# q.append([0, 0, 1])
+# count_lst[0][0] = 1
+# visit = [[]]
+
+# while q:
+    
+#     x, y, c = deque.popleft(q)
+#     visit.append(lst[x][y])
+#     for i in range(4):
+#         nx, ny = x + dx[i], y + dy[i]
+#         print(nx, ny)
+#         if 0 <= nx < n and 0 <= ny < m:
+#             print('in boder')
+#             if lst[nx][ny] not in visit and count_lst[nx][ny] <= c:
+#                 count_lst[nx][ny] = c + 1
+#                 q.append([nx, ny, c + 1])
+#                 print('in')
+
+# for i in count_lst:
+#     print(i)
