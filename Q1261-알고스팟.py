0fd01ac28528998d483635df49d9dcbc415f4296
HeoYou <lov21y_@naver.com>
2022-04-30 23:52:41 +0900

    약간 야매로 다익스트라를 만들었따.!! 벽이 없을경우 큐의 앞부분에 값을 넣음

--- Q1261-알고스팟.py
+++ Q1261-알고스팟.py
@@ -2,15 +2,15 @@ import sys
 from collections import deque
 
 input = sys.stdin.readline
-INF = int(1e9)
+INF = int(1e4)
 
 dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
 
 M, N = map(int, input().split())
 
 graph = [list(map(int, list(input().strip()))) for i in range(N)]
-visit1 = [[INF] * M for i in range(N)]
-visit1[0][0] = 0
+visit = [[INF] * M for i in range(N)]
+visit[0][0] = 0
 q = deque([(0, 0)])
 
 while q:
@@ -21,8 +21,12 @@ while q:
         nx, ny = x + dx[i], y + dy[i]
         if 0 > nx or nx >= N or 0 > ny or ny >= M:
             continue
-        if visit1[x][y] + graph[nx][ny] < visit1[nx][ny]:
-            visit1[nx][ny] = visit1[x][y] + graph[nx][ny]
-            q.append((nx, ny))
+        if visit[x][y] + graph[nx][ny] < visit[nx][ny]:
+            if graph[nx][ny] == 0:
+                visit[nx][ny] = visit[x][y]
+                q.appendleft((nx, ny))
+            else:
+                visit[nx][ny] = visit[x][y] + 1
+                q.append((nx, ny))
    
-print(visit1[N - 1][M - 1])
+print(visit[N - 1][M - 1])
