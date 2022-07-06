1b9bcf0f7b252487d76a5ac09c6a419aad335d54
HeoYou <lov21y_@naver.com>
2022-07-07 00:34:54 +0900

    그냥 테두리를 벽으로 채우고 햇다 편해편해 ..

--- Q3190-뱀.py
+++ Q3190-뱀.py
@@ -7,34 +7,51 @@ N = int(input())
 
 board = [[2] + [0] * (N) + [2] for i in range(N)]
 board = [[2] * (N + 2)]+ board + [[2] * (N + 2)]
-print(board)
+board[1][1] = 2
 
 next_dir = deque()
-snake = deque([1, 1])
+snake = deque()
+snake.append([1, 1])
 direction = 1   # 0위 1오 2아래 3왼
 
 for i in range(K := int(input())):
     a, b = map(int, input().split())
-    
-    board[a][b] = 1
+    board[b][a] = 1
 
 for i in range(int(input())):
     a, b = input().split()
     next_dir.append([int(a), b])
     
-for i in board:
-    print(i)
-    
 x, y = 1, 1
 answer = 0
 
-# while True:
-#     answer += 1
-#     x, y = x + dx[direction], y + dy[direction]
+while True:
     
-#     if board[x][y] == 1:
+    answer += 1
+    
+    x, y = x + dx[direction], y + dy[direction]
+    if next_dir and answer == next_dir[0][0]:
+        a, D = next_dir.popleft()
+        if D == "L":
+            direction = (direction - 1) % 4
+        if D == "D":
+            direction = (direction + 1) % 4
+            
+    
+    if board[x][y] == 1:
+        board[x][y] = 2
+        snake.append([x, y])
+    
+    elif board[x][y] == 0:
+        board[x][y] = 2
+        snake.append([x, y])
+        x2, y2 = snake.popleft()
+        board[x2][y2] = 0
+    
+    else:
+        break
         
-    
+print(answer)
     
     
 
