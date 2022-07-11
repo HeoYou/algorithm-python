e02a47de5ae1f230c3a7b65772ad77b9947972ff
HeoYou <lov21y_@naver.com>
2022-07-11 22:42:45 +0900

    체크하는 로직 다시

--- Q14890-경사로.py
+++ Q14890-경사로.py
@@ -1,50 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-def chk(lst):
-	chkLst = [False] * N
-	
-	before = lst[0]
-	for i in range(1, N):
-		if before == lst[i]:
-			before = lst[i]
-			
-		else:
-			if abs(before - lst[i]) > 1:
-				print(1)
-				return False
-			
-			elif before > lst[i]:
-				print("i", i)
-				count = 0
-				while count < L:
-					count += 1
-					if i + count >= N:
-						print(2)
-						return False
-					elif lst[i + count] != before - 1:
-						print(3)
-						print(i, count)
-						print(lst[i + count], lst[i])
-						return False
-						
-				for j in range(L):
-					chkLst[i + j + 1] = True
-					
-				
-			
-			if before < lst[i]:
-				count = 0
-				while count < L:
-					count += 1
-					if i - count < 0:
-						print(4)
-						return False
-					elif lst[i - count] != lst[i] - 1:
-						print(5)
-						return False	
-		print(chkLst)
-	return True
+def chk(line):
+    for i in range(1, N):
+        if abs(line[i] - line[i - 1]) > 1:
+            return False
+        if line[i] < line[i - 1]:
+            for j in range(L):
+                if i + j >= N or line[i] != line[i + j] or slope[i + j]:
+                    return False
+                if line[i] == line[i + j]:
+                    slope[i + j] = True
+        elif line[i] > line[i - 1]:
+            for j in range(L):
+                if i - j - 1 < 0 or line[i - 1] != line[i - j - 1] or slope[i - j - 1]:
+                    return False
+                if line[i - 1] == line[i - j - 1]:
+                    slope[i - j - 1] = True
+    return True
 		
 		
 
@@ -53,22 +26,14 @@ N, L = map(int, input().split())
 b = [list(map(int, input().split())) for i in range(N)]
 answer = 0
 
-# for l in b:
-# 	if chk(l):
-# 		print(l)		
-# 		answer += 1
-# print()
-# for j in range(N):
-# 	if chk([b[i][j] for i in range(N)]):
-# 		print([b[i][j] for i in range(N)])
-# 		answer += 1
-chk([2, 2, 2, 1, 1, 1])
-print(answer)		
-# lstTmp = [b[i][0] for i in range(N)]
+for i in range(N):
+    slope = [False] * N
+    if chk([b[i][j] for j in range(N)]):
+        answer += 1
 
-# print(lstTmp)
-# chk(lstTmp)
+for j in range(N):
+    slope = [False] * N
+    if chk([b[i][j] for i in range(N)]):
+        answer += 1
 
-# print(N, L)
-# for i in b:
-# 	print(i)
+print(answer)
